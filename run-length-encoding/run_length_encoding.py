26cd711d8c646311b72e067cbe3010a316fc6788
macelai <vini.macelai@hotmail.com>
2017-05-31 14:15:37 -0300

    More solutions

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,5 +1,27 @@
 def decode(word):
-    pass
+    decoded = ""
+    number = ""
+    for i in range(len(word)):
+        if word[i].isdigit():
+            number += word[i]
+        else:
+            if number == "":
+                number = "1"
+            decoded += int(number) * word[i]
+            number = ""
+    return decoded
 
 def encode(word):
-    pass
+    count = 1
+    word = word + "\n"
+    str_encoded = ""
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            count += 1
+        else:
+            if count == 1:
+                str_encoded += word[i]
+            else:
+                str_encoded += str(count) + word[i]
+            count = 1
+    return(str_encoded)
